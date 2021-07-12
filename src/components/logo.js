97f20e660f92824176095aee502ab98e936f9772
tmhuyett <toddhuyett@gmail.com>
2021-07-12 11:21:03 -0400

    'd'

---
+++ src/components/logo.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Logo = props => (
+  <div className="site-logo">
+    <Link to="/">Hanover Merchant Solutions</Link>
+  </div>
+)
+
+export default Logo
